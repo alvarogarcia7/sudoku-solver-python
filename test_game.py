2cd51dcb174e963d1ef32bb2f81e397054613ced
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-12-22 07:07:52 +0400

    Green: check sudoku is correct

--- test_game.py
+++ test_game.py
@@ -13,6 +13,9 @@ class Sudoku:
     def is_complete(self):
         return all([len(list(filter(lambda cell: cell is not None, row))) == 9 for row in self.value])
 
+    def is_correct(self):
+        return all([sorted(row) == list(range(1, 10)) for row in self.value])
+
 
 class IO:
     def load(self, raw_values):
@@ -93,6 +96,24 @@ class TestIOTest(unittest.TestCase):
         sudoku = io.load(raw_values)
 
         self.assertFalse(sudoku.is_complete())
+
+    def test_check_is_correct(self):
+        io = IO()
+        # Source: https://github.com/jimburton/sudoku/blob/master/puzzles/solved1.sud
+        raw_values = [
+            "123456789",
+            "456789123",
+            "789123456",
+            "214365897",
+            "365897214",
+            "897214365",
+            "531642978",
+            "642978531",
+            "978531642"
+        ]
+        sudoku = io.load(raw_values)
+
+        self.assertTrue(sudoku.is_correct())
 
 
 if __name__ == '__main__':
