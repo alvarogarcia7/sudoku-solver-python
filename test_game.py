725153a079c246e2e64f0a57626dd937fab6ca3c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-12-22 07:00:24 +0400

    Green: serialize/deserialize empty sudoku

--- test_game.py
+++ test_game.py
@@ -17,11 +17,12 @@ class Sudoku:
 
 class IO:
     def load(self, raw_values):
-        x: list[list[int]] = [list(map(lambda x: int(x), raw_value)) for raw_value in raw_values]
+        x: list[list[int]] = [list(map(lambda x: int(x) if x != ' ' else None, raw_value)) for raw_value in raw_values]
         return Sudoku(x)
 
     def serialize(self, sudoku: Sudoku):
-        x: list[str] = ["".join(map(lambda x: x.__str__(), raw_value)) for raw_value in sudoku.value]
+        x: list[str] = ["".join(map(lambda x: x.__str__() if x is not None else ' ', raw_value)) for raw_value in
+                        sudoku.value]
         return x
 
 
@@ -30,6 +31,23 @@ class TestIOTest(unittest.TestCase):
         io = IO()
         raw_values = [
             "123456789",
+            "123456789",
+            "123456789",
+            "123456789",
+            "123456789",
+            "123456789",
+            "123456789",
+            "123456789",
+            "123456789"
+        ]
+        sudoku = io.load(raw_values)
+        actual = io.serialize(sudoku)
+        self.assertEqual(raw_values, actual)
+
+    def test_roundtrip_loading_when_not_complete(self):
+        io = IO()
+        raw_values = [
+            " 23456789",
             "123456789",
             "123456789",
             "123456789",
